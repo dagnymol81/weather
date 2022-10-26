29dac8cf2e455dcb967d4aff0588c6d6cb442e89
Dagny Mol <dagnymol@gmail.com>
2022-10-26 19:16:05 -0400

    greeting

--- src/components/ShowPicture.js
+++ src/components/ShowPicture.js
@@ -3,6 +3,8 @@ export default function ShowPicture({ today }) {
   console.log(today.shortForecast)
 
   let weather = today.shortForecast
+  let temperature = today.temperature
+
 
   const getWeatherIcon = (weather) => {
 
@@ -25,9 +27,24 @@ export default function ShowPicture({ today }) {
 
   let icon = getWeatherIcon(weather)
 
+  const getGreeting = (weather, temperature) => {
+    switch(true) {
+      case weather.includes('Rain'):
+        return 'Remember your umbrella!'
+      case temperature < 40:
+        return 'Wear your winter coat and gloves!'
+      case weather.includes('Sunny'):
+        return('Enjoy the sun!')      
+      default:
+        return 'Have fun!'
+    }
+  }
+  let greeting = getGreeting(weather, temperature)
+
   return(
     <>
     <img src={`./images/${icon}.png`} alt="weather" />
+    {greeting}
     </>
   )
 }
